f93e4e09e7391efe2c2da140e1db1ceb320ae011
urbanec <urbanec@spsejecna.cz>
2025-02-26 11:52:51 +0100

    soubory

--- src/Map.java
+++ src/Map.java
@@ -1,7 +1,9 @@
+import javax.xml.stream.Location;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Map {
 
@@ -9,17 +11,55 @@ public class Map {
         System.out.println(world());
     }
 
-    public ArrayList<String> world() {
-        ArrayList<String> list = new ArrayList<>();
+    private HashMap<Integer, Location> map = new HashMap<>();
+    private int start = 0;
+    private int currentPosition = start;
+
+    public boolean LoadMap(){
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\world"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] lines = line.split(";");
+                Location location = new Location(
+                    lines[1],
+                    Integer.parseInt(lines[0]),
+                    Arrays.copyOfRange(lines, 2, 5)
+                );
+                world.put(Integer.valueOf(lines[0]), location);
+            }
+            return true;
+        } catch (IOException e){
+            return false;
+        }
+    }
+
+//
+//    public ArrayList<String> world1() {
+//        ArrayList<String> list = new ArrayList<>();
+//        try (BufferedReader br = new BufferedReader(new FileReader("src\\world"))) {
+//            String text;
+//            while ((text = br.readLine()) == null) {
+//                list.add(text);
+//            }
+////            return list;
+//        } catch (IOException e) {
+//
+//        }
+//        return list;
+//    }
+
+    public String world() {
+        String list = "";
         try (BufferedReader br = new BufferedReader(new FileReader("src\\world"))) {
             String text;
-            while ((text = br.readLine()).matches("\b;\b")) {
-                list.add(text);
+            while ((text = br.readLine()) != null) {
+                list += text + "\n";
             }
+            return list;
         } catch (IOException e) {
-//djfkslj;
+
         }
-        return list;
+        return "Nelze provest";
     }
 
 }
