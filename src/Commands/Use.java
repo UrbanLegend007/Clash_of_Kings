b4d4fe320ab8f9ec1bb7b217bb1a527909643083
Admin <lukykuk6@gmail.com>
2025-03-18 18:46:36 +0100

    pridani inventare do use,

--- src/Commands/Use.java
+++ src/Commands/Use.java
@@ -9,6 +9,7 @@ public class Use extends Command {
 
     private CommandManager worldCommandManager;
     Scanner scanner = new Scanner(System.in);
+    private Inventory inventory = new Inventory();
 
     public Use(CommandManager worldCommandManager) {
         this.worldCommandManager = worldCommandManager;
@@ -18,17 +19,13 @@ public class Use extends Command {
     public String execute() {
         Kingdom currentKingdom = worldCommandManager.world.get(worldCommandManager.currentPosition);
 
-        System.out.print("Enter what you want to use: \n1) krystal 1 \n2) krystal 2 \n3) krystal 3 \n");
-        String krystal = scanner.nextLine();
-
-        if(krystal.equals("1") || krystal.equals("krystal 1")){
-            return "You have used krystal 1";
-        }else if(krystal.equals("2") || krystal.equals("krystal 2")){
-            return "You have used krystal 2";
-        }else if(krystal.equals("3") || krystal.equals("krystal 3")){
-            return "You have used krystal 3";
+        System.out.println("Enter the amount of krystals you would like to use:");
+        int krystal = scanner.nextInt();
+        if(inventory.getResourceAmount(4) >= krystal){
+            inventory.removeItem(4, krystal);
+            return "You have used " + krystal + " krystals.";
         }else{
-            return "Invalid krystal";
+            return "You don't have enough krystals left";
         }
     }
 
