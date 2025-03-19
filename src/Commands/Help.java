e8f8e675d55851a9b21fb93abbef3b2175e4d583
urbanec <urbanec@spsejecna.cz>
2025-03-19 10:42:32 +0100

    vypsani help pouze v return

--- src/Commands/Help.java
+++ src/Commands/Help.java
@@ -8,24 +8,24 @@ public class Help extends Command{
 
     @Override
     public String execute() {
-        System.out.println("\n-> Travel = Muzes cestovat mezi kralovstvimi (kdyz cestujes, muzes se rozhodnout, jaka cast tve armady pujde s tebou).");
-        System.out.println("          -> Talk = Ziskas tak informace, ktere se ti muzou hodit, nebo muzes zlepsit vztahy mezi kralovstvimi");
-        System.out.println("          -> Trade = Muzes tak koupit ci prodat ruzne predmety, ci vytvorit primo obchodni dohody, pokud mas potrebny scroll, muzes dostat lepsi podminky, nebo kdyz mas s nimi alience.");
-        System.out.println("          -> Trade => Get (vyberes predmet, ktery chces dostat, pokud ti to kralovstvi povoli, nebo uzemi ziskas nasilim, tak ho dostanes, jinak ne).");
-        System.out.println("          -> Negotiation -> Peace terms = Muzes se domluvit na prijatelnych podminkach, pokud je mezi vámi nějeký konflikt.");
-        System.out.println("                         -> Aliance = Muzete se s nekterymi kralovstvimi vyhodne spojit, pokud mas potrebny scroll, muzes dostat lepsi podminky.");
-        System.out.println("                         -> Attack = Muzete vyhlasit nekteremu kralovstvi valku a prerusit tak veskere aliance a obchody.");
-        System.out.println("\n-> Maintain = Muzes vylepsit armadu.");
-        System.out.println("            -> Gear => Metal (vyberes kov, kterym chces vylepsit vybaveni, cim vzacnejsi, tim lepsi).");
-        System.out.println("            -> Properties => Resources (vyberes zdroje, kterymi chces vylepsit vlastnosti, cim vzacnejsi, tim lepsi).");
-        System.out.println("\n-> Control Army = Muzes ovladat armadu na bitevnim poli, nebo pouze na mape, aniz bys ty sam musel nekam chodit (obvykle v pripade valky).");
-        System.out.println("                -> Attack = Zautocis na armadu na bitevnim poli, tvoje armada bude odkrytejsi, ale rychlejsi a silnejsi.");
-        System.out.println("                -> Defence = Budes branit uzemi a tvoje armada bude drzet pozice, nebude se prilis hybat, ale bude vice skryta.");
-        System.out.println("                -> Reinforcements = Zavolas posili bud svoje vlastni armady, ktera se nachazi jinde, nebo nejakeho jineho kralovstvi, se kterym mas alience.");
-        System.out.println("                -> Occupy = Zaberes jistou cast uzemi, hradu nebo vyhodne pozice.");
-        System.out.println("                -> Retreat = Ustoupis na jinou cast uzemi, pripadne opustis bojiste uplne v pripade nouze.");
-        System.out.println("                -> Use = Vyberes a pouzijes krystal, ktery ti muze pomoct v boji.");
-        return "\nToto jsou vsechny prikazy, ktere muzes provest.";
+        return "\n-> Travel = Muzes cestovat mezi kralovstvimi (kdyz cestujes, muzes se rozhodnout, jaka cast tve armady pujde s tebou)." +
+                "\n          -> Talk = Ziskas tak informace, ktere se ti muzou hodit, nebo muzes zlepsit vztahy mezi kralovstvimi." +
+                "\n          -> Trade = Muzes tak koupit ci prodat ruzne predmety, ci vytvorit primo obchodni dohody, pokud mas potrebny scroll, muzes dostat lepsi podminky, nebo kdyz mas s nimi alience." +
+                "\n          -> Trade => Get (vyberes predmet, ktery chces dostat, pokud ti to kralovstvi povoli, nebo uzemi ziskas nasilim, tak ho dostanes, jinak ne)." +
+                "\n          -> Negotiation -> Peace terms = Muzes se domluvit na prijatelnych podminkach, pokud je mezi vámi nějeký konflikt." +
+                "\n                         -> Aliance = Muzete se s nekterymi kralovstvimi vyhodne spojit, pokud mas potrebny scroll, muzes dostat lepsi podminky." +
+                "\n                         -> Attack = Muzete vyhlasit nekteremu kralovstvi valku a prerusit tak veskere aliance a obchody." +
+                "\n\n-> Maintain = Muzes vylepsit armadu." +
+                "\n            -> Gear => Metal (vyberes kov, kterym chces vylepsit vybaveni, cim vzacnejsi, tim lepsi)." +
+                "\n            -> Properties => Resources (vyberes zdroje, kterymi chces vylepsit vlastnosti, cim vzacnejsi, tim lepsi)." +
+                "\n\n-> Control Army = Muzes ovladat armadu na bitevnim poli, nebo pouze na mape, aniz bys ty sam musel nekam chodit (obvykle v pripade valky)." +
+                "\n                -> Attack = Zautocis na armadu na bitevnim poli, tvoje armada bude odkrytejsi, ale rychlejsi a silnejsi." +
+                "\n                -> Defence = Budes branit uzemi a tvoje armada bude drzet pozice, nebude se prilis hybat, ale bude vice skryta." +
+                "\n                -> Reinforcements = Zavolas posili bud svoje vlastni armady, ktera se nachazi jinde, nebo nejakeho jineho kralovstvi, se kterym mas alience." +
+                "\n                -> Occupy = Zaberes jistou cast uzemi, hradu nebo vyhodne pozice." +
+                "\n                -> Retreat = Ustoupis na jinou cast uzemi, pripadne opustis bojiste uplne v pripade nouze." +
+                "\n                -> Use = Vyberes a pouzijes krystal, ktery ti muze pomoct v boji." +
+                "\n\nToto jsou vsechny prikazy, ktere muzes provest.";
     }
 
     @Override
